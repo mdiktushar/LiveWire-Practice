e4d4134d949c27c7472840e458bc79f28ee6b2bf
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-07-08 08:44:25 +0600

    searching complete

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
+use Livewire\WithPagination;
+
 
 class TodoList extends Component
 {
+    use WithPagination;
+    public $name, $search;
+
+    public function create()
+    {
+        // validate
+        $input = $this->validate([
+            'name' => 'required|min:3|max:255',
+        ]);
+        // store in database
+        Todo::create($input);
+        // clear input
+        $this->reset('name');
+        // flash message
+        session()->flash('success', 'Saved');
+    }
+
     public function render()
     {
-        return view('livewire.todo-list');
+        $todos = Todo::where('name', 'like', "%{$this->search}%")->latest()->paginate(3);
+
+        return view('livewire.todo-list', compact('todos'));
     }
 }
